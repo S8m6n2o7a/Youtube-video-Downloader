cee7ba25496611c66085ac7b500ab05f9b888915
sumon <64481672+S8m6n2o7a@users.noreply.github.com>
2021-04-15 04:51:14 +0100

    Update requirment.sh

--- requirment.sh
+++ requirment.sh
@@ -4,6 +4,11 @@
 echo "Sleeping for 5 seconds…"
 sleep 5
 sudo apt install youtube-dl
+sudo apt install python3
+sudo apt install python3-pip
+sudo -H pip install --upgrade youtube-dl
+sudo apt install toilet -y
+sudo apt install figlet -y
 echo "Completed"
 chmod 777 yt-meadia.py
 ./yt-meadia.py
